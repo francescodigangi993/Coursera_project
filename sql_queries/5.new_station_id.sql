f1872cc333ee99f5885ac80a63adb9eac6654536
francescodigangi993 <francesco.digangi993@gmail.com>
2026-02-24 18:36:02 +0100

    Update 5.new_station_id.sql

--- sql_queries/5.new_station_id.sql
+++ sql_queries/5.new_station_id.sql
@@ -1,4 +1,5 @@
---Questa query crea la tabella con il nuovo id delle stazioni
+--This query create the new table for the station with new progressive numeric id
+
 CREATE TABLE new_station_id AS
 SELECT 
     row_number() OVER (ORDER BY station_name) AS station_id,
@@ -8,4 +9,5 @@ FROM (
     SELECT start_station_name AS station_name FROM annual_rides WHERE start_station_name IS NOT NULL
     UNION
     SELECT end_station_name AS station_name FROM annual_rides WHERE end_station_name IS NOT NULL
+
 ) AS station_names;
