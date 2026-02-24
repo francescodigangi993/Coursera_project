35d871953a992e6fd0f593913d7b280fe260f0a0
francescodigangi993 <francesco.digangi993@gmail.com>
2026-02-24 18:35:15 +0100

    Update 4.find_station_outliers.sql

--- sql_queries/4.find_station_outliers.sql
+++ sql_queries/4.find_station_outliers.sql
@@ -1,6 +1,6 @@
---Questa query crea due tabelle con le stazioni di andata e di ritorno che vengono unite 
---con un FULL OUTER JOIN per vedere quali stazioni non hanno corrispondenze tra le due
---liste
+-- This query creates two tables for departure and arrival stations, which are then combined 
+-- using a FULL OUTER JOIN to identify any stations that do not have matches between the two lists.
+
 WITH t1 AS (SELECT DISTINCT start_station_name
 			FROM annual_rides),
 	 t2 AS (SELECT DISTINCT end_station_name
@@ -9,5 +9,6 @@ SELECT *
 FROM t1
 FULL OUTER JOIN t2 ON t1.start_station_name=t2.end_station_name
 WHERE end_station_name IS NULL OR start_station_name IS NULL
---Abbiamo trovato 2 stazioni che figurano solo in start e una solo in end. Questo spiega
---anche la discrepanza nel conteggio dei dati
+
+--Discovered two stations unique to 'start_station' and one unique to 'end_station', 
+--explaining the mismatch in the total counts.
