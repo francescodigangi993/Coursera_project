b90ba0b451c3e7d99a4d6febab6b4aa00f4bd546
francescodigangi993 <francesco.digangi993@gmail.com>
2026-02-24 18:39:59 +0100

    Update 10.tableau_export.sql

--- sql_queries/10.tableau_export.sql
+++ sql_queries/10.tableau_export.sql
@@ -1,6 +1,6 @@
---Questa query mi permettera di esportare su tableau solo i dati effettivamente importanti
---Poiche non abbiamo un id_utente non abbiamo bisogno di analizzare l etratte, perche non
---possiamo sapere se i member fanno sempre la stessa strada ad esempio
+--This query exports only the essential data for Tableau. Since unique User IDs are unavailable, 
+--route-level analysis (specific paths) is excluded as it is impossible to track individual user behavior or recurring patterns.
+
 CREATE TABLE tableau_export AS
 SELECT 
     start_date,
@@ -11,4 +11,5 @@ SELECT
     COUNT(*) AS total_rides,
     ROUND(AVG(duration_minutes), 2) AS avg_duration
 FROM annual_rides_3
+
 GROUP BY 1, 2, 3, 4, 5;
